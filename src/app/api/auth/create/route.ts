8d4a2820fd6946899e4504b3cd1748f1465774a9
iamzainn <iamzainnf@gmail.com>
2024-06-20 23:20:42 +0500

    Add Stripe Integration

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -5,6 +5,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
+import { stripe } from "@/lib/Stripe";
 
 export async function GET() {
   
@@ -15,20 +16,48 @@ export async function GET() {
     throw new Error("Something went wrong...");
   }
 
+  
+
+
+
+
+
+
   let dbUser = await prisma.user.findUnique({
     where: {
       id: user.id,
     },
+    select:{
+      id: true,
+      stripeCustomerId:true
+    }
   });
 
+  if (dbUser && !dbUser.stripeCustomerId) {
+
+    const stripeCustomer = await stripe.customers.create({
+      email: user.email as string,
+    });
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        stripeCustomerId: stripeCustomer.id,
+      },
+    });
+  }
   if (!dbUser) {
+    const stripeCustomer = await stripe.customers.create({
+      email: user.email as string,
+    });
     
     dbUser = await prisma.user.create({
       data: {
         id: user.id,
         email:user.email ?? "",
         name:user.given_name ?? "",
-      
+        stripeCustomerId: stripeCustomer.id,
       },
     });
   }
--- src/components/Submitbuttons.tsx
+++ src/components/Submitbuttons.tsx
@@ -20,3 +20,40 @@ export function SubmitButton() {
     </>
   );
 }
+
+export function StripeSubscriptionCreationButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-full">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button type="submit" className="w-full">
+          Create Subscription
+        </Button>
+      )}
+    </>
+  );
+}
+
+export function StripePortal() {
+  const { pending } = useFormStatus();
+
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" /> Please Wait
+        </Button>
+      ) : (
+        <Button className="w-fit" type="submit">
+          View payment details
+        </Button>
+      )}
+    </>
+  );
+}
+
