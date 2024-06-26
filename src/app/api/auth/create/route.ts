ccbf7ebc6288124cf61e7ea3f37a1e2303b8cb51
iamzainn <iamzainnf@gmail.com>
2024-06-26 14:43:24 +0500

    fix error

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -12,59 +12,70 @@ export async function GET() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    throw new Error("Something went wrong...");
-  }
-
-  
-
-
-
-
-
-
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-    select:{
-      id: true,
-      stripeCustomerId:true
+  try{
+    if (!user || user === null || !user.id) {
+      throw new Error("Something went wrong...");
     }
-  });
-
-  if (dbUser && !dbUser.stripeCustomerId) {
-
-    const stripeCustomer = await stripe.customers.create({
-      email: user.email as string,
-    });
-    await prisma.user.update({
+  
+    
+  
+  
+  
+  
+  
+  
+    let dbUser = await prisma.user.findUnique({
       where: {
         id: user.id,
       },
-      data: {
-        stripeCustomerId: stripeCustomer.id,
-      },
+      select:{
+        id: true,
+        stripeCustomerId:true
+      }
     });
+  
+    if (dbUser && !dbUser.stripeCustomerId) {
+  
+      const stripeCustomer = await stripe.customers.create({
+        email: user.email as string,
+      });
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          stripeCustomerId: stripeCustomer.id,
+        },
+      });
+    }
+    if (!dbUser) {
+      const stripeCustomer = await stripe.customers.create({
+        email: user.email as string,
+      });
+      
+      dbUser = await prisma.user.create({
+        data: {
+          id: user.id,
+          email:user.email ?? "",
+          name:user.given_name ?? "",
+          stripeCustomerId: stripeCustomer.id,
+        },
+      });
+    }
+  
+    return NextResponse.redirect(
+      
+         "http://localhost:3000/dashboard"
+        
+    );
   }
-  if (!dbUser) {
-    const stripeCustomer = await stripe.customers.create({
-      email: user.email as string,
-    });
-    
-    dbUser = await prisma.user.create({
-      data: {
-        id: user.id,
-        email:user.email ?? "",
-        name:user.given_name ?? "",
-        stripeCustomerId: stripeCustomer.id,
-      },
-    });
+    catch(e){
+     console.log("unable to logged in " + e);
+     return NextResponse.redirect(
+      
+      "http://localhost:3000"
+     
+ );
+    }
   }
 
-  return NextResponse.redirect(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/dashboard"
-      : "https://marshal-ui-yt.vercel.app/"
-  );
-}
