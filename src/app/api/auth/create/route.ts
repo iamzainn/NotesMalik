8028938871b5690e006fd503f0c666dad2bc861c
iamzainn <iamzainnf@gmail.com>
2024-06-26 15:17:46 +0500

    fix caching

--- src/app/api/auth/create/route.ts
+++ src/app/api/auth/create/route.ts
@@ -6,9 +6,10 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 import { stripe } from "@/lib/Stripe";
+import  {unstable_noStore as noStore} from 'next/cache';
 
 export async function GET() {
-  
+  noStore();
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
